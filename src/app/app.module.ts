f496938925ebed9f99697a80d1a70ed5a0794183
v.abramov <valeriy.abramov@faceit.com.ua>
2018-03-19 09:03:55 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
