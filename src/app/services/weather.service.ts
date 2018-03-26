79ddb712f86fdd80661b174c96feb2761085f270
v.abramov <valeriy.abramov@faceit.com.ua>
2018-03-26 10:37:49 +0300

    add comments in home.component.html

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Subject} from 'rxjs/Subject';
 
 @Injectable()
 export class WeatherService {
-  readonly ROOT_URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+  readonly ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?q=';
   readonly apikey = 'APPID=540a4f8c1e5c2e1b26a3720d3994ad63';
 
   public searchCity$: Subject<any> = new Subject();
@@ -32,3 +32,31 @@ export class WeatherService {
   "name": "London",
   "cod": 200
 }*/
+
+/*
+forecast
+
+{"city":{"id":1851632,"name":"Shuzenji",
+  "coord":{"lon":138.933334,"lat":34.966671},
+  "country":"JP",
+    "cod":"200",
+    "message":0.0045,
+    "cnt":38,
+    "list":[{
+    "dt":1406106000,
+    "main":{
+      "temp":298.77,
+      "temp_min":298.77,
+      "temp_max":298.774,
+      "pressure":1005.93,
+      "sea_level":1018.18,
+      "grnd_level":1005.93,
+      "humidity":87,
+      "temp_kf":0.26},
+    "weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],
+    "clouds":{"all":88},
+    "wind":{"speed":5.71,"deg":229.501},
+    "sys":{"pod":"d"},
+    "dt_txt":"2014-07-23 09:00:00"}
+  ]}
+*/
